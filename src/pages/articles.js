da3a92a9cd4da79693943fc7f9addbe42344be90
rucpata <rucinska.patrycja@gmail.com>
2021-07-07 17:03:51 +0200

    gatsby - training blog

---
+++ src/pages/articles.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Articles() {
+    return (
+        <h1>
+            Articless
+        </h1>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "gatsby";
+
+
+export default function Home() {
+  return (
+    <section>
+      <h1>Hello world!</h1>
+      <Link to='articles'>Articles</Link>
+    </section>
+  ) 
+}
